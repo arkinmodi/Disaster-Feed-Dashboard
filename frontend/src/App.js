03a6d026f1499fa4a61c69fcf51d85c79485c92a
Arkin Modi <arkinmodi@gmail.com>
2020-10-30 21:56:38 -0400

    Created Live Feed component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,69 +1,86 @@
-import React from 'react';
-import './App.css';
-import {
-  HashRouter as Router,
-  Route,
-  NavLink
-} from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { HashRouter as Router, Route, NavLink } from "react-router-dom";
 
 // Install the socket io client using:
 //    npm install socket.io
 //
 // Then import socket io and create a socket:
 //
-import io from 'socket.io-client';
-const socket = io('http://localhost:3001');
+import io from "socket.io-client";
+const socket = io("http://localhost:3001");
 //
 // See: https://socket.io/get-started/chat
 //      https://www.npmjs.com/package/socket.io-client
 
+class LiveFeed extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.posts.map(
+          ({ name, image, content, problem, priority, id }) => (
+            <div key={id}>
+              <img src={image} alt="{name}" /> <br />
+              {name} <br />
+              {problem} <br />
+              {priority} <br />
+              {content} <br /> <br /> <br /> <br />
+            </div>
+          )
+        )}
+      </div>
+    );
+  }
+}
 
 class App extends React.Component {
-
   constructor(props) {
     super(props);
 
     // An array of social media posts messages, and we'll increment nextID
     // to maintain a unique ID for each post in our array
-    this.state = {posts: [],nextID: 0};
+    this.state = { posts: [], nextID: 0 };
 
     // We can setup the event handlers for the messages in the constructor...
-    socket.on('connect', function(){
+    socket.on(
+      "connect",
+      function () {
+        console.log("Connect....");
 
-      console.log("Connect....");
+        // When we receive a social media message, call setState and insert
+        // it into the array of posts
+        socket.on(
+          "post",
 
-      // When we receive a social media message, call setState and insert 
-      // it into the array of posts
-      socket.on('post', 
+          function (data) {
+            // Can uncomment this to see the raw data coming in...
+            // console.log("post: " + JSON.stringify(data));
 
-        function(data) {
-
-          // Can uncomment this to see the raw data coming in...
-          // console.log("post: " + JSON.stringify(data));
-
-          // increment the next unique ID, and put post data into the list of 
-          // posts... use the '...' syntax to make this insertion easier:
-          // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax 
-          this.setState( 
-            {posts: [...this.state.posts,
-                     {name: data.name, 
-                      image: data.image, 
-                      content: data.content, 
-                      problem: data.problem,
-                      priority: data.priority,
-                      id: this.state.nextID}]
-            ,nextID: this.state.nextID + 1} );
-        }.bind(this));
-        // ^^ Like other event handlers, we bind the callback function to the 
-        // component so we can use setState        
-
-    }.bind(this));
+            // increment the next unique ID, and put post data into the list of
+            // posts... use the '...' syntax to make this insertion easier:
+            // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+            this.setState({
+              posts: [
+                ...this.state.posts,
+                {
+                  name: data.name,
+                  image: data.image,
+                  content: data.content,
+                  problem: data.problem,
+                  priority: data.priority,
+                  id: this.state.nextID,
+                },
+              ],
+              nextID: this.state.nextID + 1,
+            });
+          }.bind(this)
+        );
+        // ^^ Like other event handlers, we bind the callback function to the
+        // component so we can use setState
+      }.bind(this)
+    );
     // ^^ ... And same with the callback function for when a connection is made
-
   }
-  
-
-
 
   render() {
     return (
@@ -74,38 +91,15 @@ class App extends React.Component {
           <NavLink to="/analytics">Analytics</NavLink>
 
           <Route path="/home" />
-          <Route path="/live-feed" />
+          <Route
+            path="/live-feed"
+            render={(props) => <LiveFeed {...props} posts={this.state.posts} />}
+          />
           <Route path="/analytics" />
         </Router>
       </div>
-    )
+    );
   }
-
-
-
-
-
-
-
-
-
-  // // Output all the posts into a table
-  // render() {
-  //   return (
-  //     <div>      
-  //       {this.state.posts.map( 
-  //         ({name,image,content,problem,priority,id}) => 
-  //           <div key={id}> 
-  //             <img src={image} alt="{name}" /> <br />
-  //               {name} <br />
-  //               {problem} <br />
-  //               {priority} <br />
-  //               {content} <br /> <br /> <br /> <br />
-  //           </div> 
-  //         )}
-  //     </div>
-  //   );
-  // }
 }
 
 export default App;
