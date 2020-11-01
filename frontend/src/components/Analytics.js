ee911f7a54573475f6bfd61ed6f59f2ee3130e47
Arkin Modi <arkinmodi@gmail.com>
2020-10-31 22:16:07 -0400

    Populated table with values

--- frontend/src/components/Analytics.js
+++ frontend/src/components/Analytics.js
@@ -16,12 +16,50 @@ class Analytics extends React.Component {
       dangerFlood: true,
       dangerPower: true,
       dangerMedical: true,
+
+      // Array of Totals
+      // danger = danger type
+      // levelTotals = [Total Low, Total Medium, Total High, Total Critical]
+      data: [
+        { danger: "Fire", levelTotals: [0, 0, 0, 0] },
+        { danger: "Flood", levelTotals: [0, 0, 0, 0] },
+        { danger: "Power", levelTotals: [0, 0, 0, 0] },
+        { danger: "Medical", levelTotals: [0, 0, 0, 0] },
+      ],
     };
   }
 
+  getLevelTotal(level) {
+    let total = 0;
+    for (let i = 0; i < this.state.data.length; i++) {
+      total += this.state.data[i].levelTotals[level];
+    }
+    return total;
+  }
+
+  getDangerTotal(danger) {
+    let total = 0;
+    for (let i = 0; i < this.state.data.length; i++) {
+      total += this.state.data[danger].levelTotals[i];
+    }
+    return total;
+  }
+
   render() {
+    // Levels Total Column Values
+    let lowRowTotal = this.getLevelTotal(0);
+    let mediumRowTotal = this.getLevelTotal(1);
+    let highRowTotal = this.getLevelTotal(2);
+    let criticalRowTotal = this.getLevelTotal(3);
+
+    // Danger Total Row Values
+    let fireColTotal = this.getDangerTotal(0);
+    let floodColTotal = this.getDangerTotal(1);
+    let powerColTotal = this.getDangerTotal(2);
+    let medicalColTotal = this.getDangerTotal(3);
+
     return (
-      <div>
+      <div style={{ padding: "1%" }}>
         <h1>Analytics Page</h1>
 
         {/* Filter Options */}
@@ -139,43 +177,45 @@ class Analytics extends React.Component {
           <tbody>
             <tr>
               <th>Low</th>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
+              <td>{this.state.data[0].levelTotals[0]}</td>
+              <td>{this.state.data[1].levelTotals[0]}</td>
+              <td>{this.state.data[2].levelTotals[0]}</td>
+              <td>{this.state.data[3].levelTotals[0]}</td>
+              <td>{lowRowTotal}</td>
             </tr>
             <tr>
               <th>Medium</th>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
+              <td>{this.state.data[0].levelTotals[0]}</td>
+              <td>{this.state.data[1].levelTotals[0]}</td>
+              <td>{this.state.data[2].levelTotals[0]}</td>
+              <td>{this.state.data[3].levelTotals[0]}</td>
+              <td>{mediumRowTotal}</td>
             </tr>
             <tr>
               <th>High</th>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
+              <td>{this.state.data[0].levelTotals[0]}</td>
+              <td>{this.state.data[1].levelTotals[0]}</td>
+              <td>{this.state.data[2].levelTotals[0]}</td>
+              <td>{this.state.data[3].levelTotals[0]}</td>
+              <td>{highRowTotal}</td>
             </tr>
             <tr>
               <th>Critical</th>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
+              <td>{this.state.data[0].levelTotals[0]}</td>
+              <td>{this.state.data[1].levelTotals[0]}</td>
+              <td>{this.state.data[2].levelTotals[0]}</td>
+              <td>{this.state.data[3].levelTotals[0]}</td>
+              <td>{criticalRowTotal}</td>
             </tr>
             <tr>
               <th>Total</th>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
+              <td>{fireColTotal}</td>
+              <td>{floodColTotal}</td>
+              <td>{powerColTotal}</td>
+              <td>{medicalColTotal}</td>
+              <td>
+                {fireColTotal + floodColTotal + powerColTotal + medicalColTotal}
+              </td>
             </tr>
           </tbody>
         </Table>
