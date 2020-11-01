3ff4870f084d74b2ea8ef6f7db8214b89b11afac
Arkin Modi <arkinmodi@gmail.com>
2020-10-31 23:00:40 -0400

    Removed filter options

--- frontend/src/components/Analytics.js
+++ frontend/src/components/Analytics.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Table, Form } from "react-bootstrap";
+import { Table } from "react-bootstrap";
 
 class Analytics extends React.Component {
   constructor(props) {
@@ -65,6 +65,7 @@ class Analytics extends React.Component {
   }
 
   render() {
+    // Refresh the data
     this.updateData();
 
     // Levels Total Column Values
@@ -82,106 +83,6 @@ class Analytics extends React.Component {
     return (
       <div style={{ padding: "1%" }}>
         <h1>Analytics Page</h1>
-
-        {/* Filter Options */}
-        <Table borderless>
-          <thead>
-            <tr>
-              <th>Danger</th>
-              <th>Level</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <Form.Check
-                  type="checkbox"
-                  label="Fire"
-                  checked={this.state.dangerFire}
-                  onChange={(event) =>
-                    this.setState({ dangerFire: !this.state.dangerFire })
-                  }
-                />
-              </td>
-              <td>
-                <Form.Check
-                  type="checkbox"
-                  label="Low"
-                  checked={this.state.levelLow}
-                  onChange={(event) =>
-                    this.setState({ levelLow: !this.state.levelLow })
-                  }
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <Form.Check
-                  type="checkbox"
-                  label="Flood"
-                  checked={this.state.dangerFlood}
-                  onChange={(event) =>
-                    this.setState({ dangerFlood: !this.state.dangerFlood })
-                  }
-                />
-              </td>
-              <td>
-                <Form.Check
-                  type="checkbox"
-                  label="Medium"
-                  checked={this.state.levelMedium}
-                  onChange={(event) =>
-                    this.setState({ levelMedium: !this.state.levelMedium })
-                  }
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <Form.Check
-                  type="checkbox"
-                  label="Power"
-                  checked={this.state.dangerPower}
-                  onChange={(event) =>
-                    this.setState({ dangerPower: !this.state.dangerPower })
-                  }
-                />
-              </td>
-              <td>
-                <Form.Check
-                  type="checkbox"
-                  label="High"
-                  checked={this.state.levelHigh}
-                  onChange={(event) =>
-                    this.setState({ levelHigh: !this.state.levelHigh })
-                  }
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <Form.Check
-                  type="checkbox"
-                  label="Medical"
-                  checked={this.state.dangerMedical}
-                  onChange={(event) =>
-                    this.setState({ dangerMedical: !this.state.dangerMedical })
-                  }
-                />
-              </td>
-              <td>
-                <Form.Check
-                  type="checkbox"
-                  label="Critical"
-                  checked={this.state.levelCritical}
-                  onChange={(event) =>
-                    this.setState({ levelLow: !this.state.levelCritical })
-                  }
-                />
-              </td>
-            </tr>
-          </tbody>
-        </Table>
 
         {/* Data Table */}
         <Table bordered hover>
