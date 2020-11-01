65a10ed079a9591d011fc8674249394202c4ffde
Arkin Modi <arkinmodi@gmail.com>
2020-11-01 16:12:25 -0500

    Added explaination of Live Feed & Analytics

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,8 +3,23 @@ import React from "react";
 class Home extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Home Page</h1>
+      <div style={{ padding: "1%" }}>
+        <h1>Welcome to the Disaster Feed Dashboard!</h1>
+        <br />
+        <h2>What is the Live Feed?</h2>
+        <p>
+          The Live Feed page displays a live feed of all individual posts. These
+          posts can be filtered by problem type and priority level.
+        </p>
+        <br />
+        <h2>What is the Analytics?</h2>
+        <p>
+          The Analytics page contains a table displaying the number of posts
+          under a give category. Categories include the four problem types
+          (fire, flood, power, and medical) and the four priority levels (low,
+          medium, high, and critical). This table also includes totals for each
+          row or column.
+        </p>
       </div>
     );
   }
